77ce0cd11d326396adb3e94e88e0aeabcd3f9b63
Zihad550 <hussainmuzahid83@gmail.com>
2022-03-22 07:40:56 +0600

    images optimized & deployed with basic authentication & react & firebase

--- src/Pages/Details/ToLetBanner/ToLetBanner.js
+++ src/Pages/Details/ToLetBanner/ToLetBanner.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../../../components/Button';
 import Title from '../../../components/Title';
-import src from '../../../images/toLet/banner.jpg';
+import src from '../../../images/banner.jpg';
 
 function ToLetBanner() {
     const navigate = useNavigate();
